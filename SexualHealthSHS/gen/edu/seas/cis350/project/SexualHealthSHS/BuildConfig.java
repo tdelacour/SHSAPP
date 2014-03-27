342a6217843e6624caa671c7691cae1156f2c0fb
edcai <2012ecai@gmail.com>
2014-03-27 02:07:18 -0400

    Android App Start
    This is the start of the project

---
+++ SexualHealthSHS/gen/edu/seas/cis350/project/SexualHealthSHS/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package edu.seas.cis350.project.SexualHealthSHS;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
