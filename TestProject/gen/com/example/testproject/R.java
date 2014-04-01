18f53fcd11d3981ef38a1332d224dd03feadd9f1
thomasdelacour <tdelac@seas.upenn.edu>
2014-03-31 21:22:17 -0400

    Question switching implemented

--- TestProject/gen/com/example/testproject/R.java
+++ TestProject/gen/com/example/testproject/R.java
@@ -25,7 +25,10 @@ or to a theme attribute in the form "<code>?[<i>package</i>:][<i>type</i>:]<i>na
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int dummy_button=0x7f070002;
+        public static final int answer1=0x7f070002;
+        public static final int answer2=0x7f070003;
+        public static final int answer3=0x7f070004;
+        public static final int answer4=0x7f070005;
         public static final int fullscreen_content=0x7f070000;
         public static final int fullscreen_content_controls=0x7f070001;
     }
--- TestProject/src/com/example/testproject/FullscreenActivity.java
+++ TestProject/src/com/example/testproject/FullscreenActivity.java
@@ -1,6 +1,5 @@
 package com.example.testproject;
-
-import com.example.testproject.util.SystemUiHider;
+import com.example.testproject.util.*;
 
 import android.annotation.TargetApi;
 import android.app.Activity;
@@ -9,6 +8,8 @@ import android.os.Bundle;
 import android.os.Handler;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
@@ -17,19 +18,80 @@ import android.view.View;
  * @see SystemUiHider
  */
 public class FullscreenActivity extends Activity {
+	
+	private QuestionI curQuestion;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		setContentView(R.layout.activity_fullscreen);
-
-		final View controlsView = findViewById(R.id.fullscreen_content_controls);
-		final View contentView = findViewById(R.id.fullscreen_content);
+		
+		makeTree(); // build story tree
+		setContentView(R.layout.activity_fullscreen); //XML layout 
+		setViews(); //Initialize question and answer fields
 	}
 
 	@Override
 	protected void onPostCreate(Bundle savedInstanceState) {
 		super.onPostCreate(savedInstanceState);
+	}
+	
+	// reaction to button click. Again can we generalize?
+	public void onAnswer1(View view){
+		curQuestion = curQuestion.getNextQuestions()[0];
+		setViews();
+	}
+	
+	public void onAnswer2(View view){
+		curQuestion = curQuestion.getNextQuestions()[1];
+		setViews();
+	}
+	
+	public void onAnswer3(View view){
+		curQuestion = curQuestion.getNextQuestions()[2];
+		setViews();
+	}
+	
+	public void onAnswer4(View view){
+		curQuestion = curQuestion.getNextQuestions()[3];
+		setViews();
+	}
+	
+	// reset layout
+	private void setViews(){
+		// question
+		TextView question = (TextView) findViewById(R.id.fullscreen_content);
+		question.setText(curQuestion.getQuestion());
+		
+		// answers: concatenates curQuestion answers with default ("A", "B", etc)
+		/* So I generalized QuestionI objects to be able to take any number 
+		 * of answers, but realize here that this is sort of set by the total
+		 * number of buttons we define in the XML itself. Ideas for generalizing?
+		 *  -TD
+		 */
+		String[] strArr = curQuestion.getAnswers(); 		
+		Button mButton=(Button)findViewById(R.id.answer1);
+        mButton.setText(strArr[0]);
+        mButton=(Button)findViewById(R.id.answer2);
+        mButton.setText(strArr[1]);
+        mButton=(Button)findViewById(R.id.answer3);
+        mButton.setText(strArr[2]);
+        mButton=(Button)findViewById(R.id.answer4);
+        mButton.setText(strArr[3]);
+	}
+	
+	private void makeTree(){
+		// here is where we play around with different question formulations
+		String question = "This is our second question";
+		String[] answers2 = {"A. q2 answer1", "B. q2 answer2", "C. q2 answer3", 
+		"D. q2 answer4"};
+		QuestionI question2 = new Question(question, answers2);
+		
+		question = "This is our first question";
+		String[] answers1 = {"A. q1 answer1", "B. q1 answer2", "C. q1 answer3", 
+				"D. q1 answer4"};
+		QuestionI[] nextQs1 = {question2, question2, question2, question2};
+		QuestionI question1 = new Question(question, answers1, nextQs1);
+		
+		curQuestion = question1;	
 	}
 }
