9132b518636ab97e79e024ea1a12db9799af2ff4
thomasdelacour <tdelac@seas.upenn.edu>
2014-04-02 23:26:40 -0400

    played around a bit more with health bar

--- TestProject/src/com/example/testproject/HealthView.java
+++ TestProject/src/com/example/testproject/HealthView.java
@@ -30,15 +30,22 @@ public class HealthView extends View {
 		legal = 50;
 	}
 	
+	@Override
 	public void onDraw(Canvas c){
+		//retrieve padding values
+		int top = getPaddingTop();
+		int border = 5;
+		
+		//draw border
 		paint.setColor(Color.BLACK);
 		paint.setStyle(Paint.Style.STROKE);
-		paint.setStrokeWidth(2);
-		c.drawRect(10, 10, 110, 40, paint);	
+		paint.setStrokeWidth(border);
+		c.drawRect(0, top, 100 + 2*border, top + 30, paint);	
 	
+		//draw fill
 		paint.setColor(Color.RED);
 		paint.setStyle(Paint.Style.FILL);
-		c.drawRect(12, 12, 10 + pregnancy, 38, paint);
+		c.drawRect(0, top + border, border + pregnancy, top + 30 - border, paint);
 	}
 	
 	public void updatePreg(int x){
@@ -51,5 +58,41 @@ public class HealthView extends View {
 	
 	public void updateLegal(int x){
 		legal += x;
+	}
+	
+	//yay google...
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+	    int desiredWidth = 200;
+	    int desiredHeight = 160;
+
+	    int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+	    int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+	    int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+	    int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+	    int width;
+	    int height;
+
+	    //Measure Width
+	    if (widthMode == MeasureSpec.EXACTLY) {
+	        width = widthSize;
+	    } else if (widthMode == MeasureSpec.AT_MOST) {
+	        width = Math.min(desiredWidth, widthSize);
+	    } else {
+	        width = desiredWidth;
+	    }
+
+	    //Measure Height
+	    if (heightMode == MeasureSpec.EXACTLY) {
+	        height = heightSize;
+	    } else if (heightMode == MeasureSpec.AT_MOST) {
+	        height = Math.min(desiredHeight, heightSize);
+	    } else {
+	        height = desiredHeight;
+	    }
+
+	    setMeasuredDimension(width, height);
 	}
 }
